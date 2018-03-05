6448564316663790a8c36853da18b00e8246dc5e
hollowtree <hollowtree@gmx.cn>
2018-03-05 21:42:23 +0800

    add music

---
+++ client/src/pages/pageMusic.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <navigation-bar></navigation-bar>
+        <ht-music :musicList="musicList"></ht-music>
+    </div>
+</template>
+<script>
+import NavigationBar from '@/components/NavigationBar'
+
+import htMusic from '@/components/htMusicPlayer'
+
+export default {
+    components: {
+        NavigationBar,
+        htMusic
+    },
+    data() {
+        return {
+            musicList: []
+        }
+    },
+    created() {
+        fetch('/static/data/music.json')
+            .then(res => res.json())
+            .then(data => {
+                data.forEach(val => {
+                    val.audioSrc = `http://music.smalltree.me/${val.file_name}.mp3`
+                })
+                this.musicList = data
+            })
+    }
+}
+</script>
+
---
+++ client/src/store/modules/music.js
@@ -0,0 +1,7 @@
+const state = {
+    show: false,
+    data: {}
+}
+export default {
+    state
+}
